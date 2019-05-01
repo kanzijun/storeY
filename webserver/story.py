1062218c2bec40eb58cf9817573766b92246776c
kanzijun <kanzijun@gmail.com>
2019-05-01 11:55:32 -0700

    timer function

--- webserver/story.py
+++ webserver/story.py
@@ -3,7 +3,35 @@ import json
 from grammarbot import GrammarBotClient
 import MySQLdb
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(time_out_user,'interval', minutes=10)
+sched.start()
+
+def time_out_user():
+	db = MySQLdb.connect("mysql-server", "root", "secret", "mydb")
+	cursor = db.cursor()
+
+	current_user = row[2]
+	cursor.execute("SELECT * FROM stories;")
+	rows = cursor.fetchall()
+	for row in rows:
+		#get current user
+		cursor.execute("SELECT current_ip_addr FROM stories WHERE title = %s", (row[0],))
+		current_user = cursor.fetchone()
+
+		#get valid user
+		cursor.execute("SELECT ip_addr FROM ip WHERE title = %s and ip_addr > %s ORDER BY ip_addr LIMIT 1;", (row[0], current_user))
+		valid_user = cursor.fetchone()
+
+		#update current user value to valid user
+		cursor.execute("UPDATE stories SET current_ip_addr = %s WHERE title = %s", (valid_user, row[0]))
+		db.commit()
+		db.close()	
+
 
 
 @app.route('/story/start', methods=["POST"])
@@ -77,11 +105,26 @@ def edit_story(title):
 	cursor.execute("SELECT * FROM stories WHERE title = %s", (title,))
 	row = cursor.fetchone()
 	current_user = row[2]
+
 	#if user is new to story, add user to table ip
 	cursor.execute("INSERT INTO ip SELECT * FROM (SELECT %s, %s) AS tmp WHERE NOT EXISTS (SELECT * FROM ip WHERE title = %s and ip_addr = %s) LIMIT 1;", (title, user_ip, title, user_ip))
 	
+	#get list of users writing title
 	cursor.execute("SELECT ip_addr FROM ip WHERE title = %s", (title,))
 	users = cursor.fetchall()
+
+	#get valid user
+	cursor.execute("SELECT ip_addr FROM ip  WHERE title = %s and ip_addr > %s ORDER BY ip_addr LIMIT 1;", (title, current_user))
+	valid_user = cursor.fetchone()
+
+	if user_ip != valid_user:
+		data = { "Error": "It is not your turn." }
+		resp = Response(json.dumps(data), status=200, mimetype='application/json')
+		return resp
+
+
+
+
 
 
 
@@ -105,3 +148,5 @@ def check_grammar_bot(text):
 	if len(res.matches)==0:
 		return True
 	return False
+
+
